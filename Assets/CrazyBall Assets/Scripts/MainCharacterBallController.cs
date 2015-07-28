fdc8d64b10f18cd5e06ad67e7904348e47aeb1fd
Guybrush Threepwood <guibrush001@gmail.com>
2015-07-29 00:28:23 +0200

    First ball version

---
+++ Assets/CrazyBall Assets/Scripts/MainCharacterBallController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterBallController : MonoBehaviour
+{
+    public float InitialVelocity;
+
+    private Rigidbody2D Rigidbody2DComponent;
+
+    private Vector2 PreviousVelocity;
+    private bool BallLaunched;
+
+    public void LaunchBall()
+    {
+        Rigidbody2DComponent.velocity = new Vector2(0, InitialVelocity);
+        BallLaunched = true;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        Rigidbody2DComponent = GetComponent<Rigidbody2D>();
+        BallLaunched = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PreviousVelocity = Rigidbody2DComponent.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (Rigidbody2DComponent && BallLaunched)
+        {
+            Vector2 ExitMovement = Vector2.zero;
+            if (coll.gameObject.name == "MainCharacter")
+            {
+                Vector2 BallInitialPosition = coll.gameObject.GetComponent<MainCharacterController>().GetBallInitialPosition();
+                Vector2 ContactPoint = coll.contacts[0].point;
+                float PointDistance = ContactPoint.x - BallInitialPosition.x;
+
+                if (PointDistance > 0.5f)
+                {
+                    float radAngle = 45f * Mathf.Deg2Rad;
+                    ExitMovement = new Vector2(Mathf.Cos(radAngle), Mathf.Sin(radAngle));
+                }
+                else if (PointDistance < -0.5f)
+                {
+                    float radAngle = 135f * Mathf.Deg2Rad;
+                    ExitMovement = new Vector2(Mathf.Cos(radAngle), Mathf.Sin(radAngle));
+                }
+                else
+                {
+                    ExitMovement = coll.contacts[0].normal;
+                }
+            }
+            else
+            {
+                Vector2 ContactNormal = new Vector2(coll.contacts[0].normal.x, coll.contacts[0].normal.y);
+                Vector2 MovementNormal = PreviousVelocity.normalized;
+
+                ExitMovement = Vector2.Reflect(MovementNormal, ContactNormal);
+            }
+
+            Rigidbody2DComponent.velocity = ExitMovement * InitialVelocity;
+        }
+    }
+}
--- Assets/CrazyBall Assets/Scripts/MainCharacterController.cs
+++ Assets/CrazyBall Assets/Scripts/MainCharacterController.cs
@@ -5,14 +5,30 @@ using UnityStandardAssets.CrossPlatformInput;
 public class MainCharacterController : MonoBehaviour
 {
     public float MaxVelocity;
-    public GameObject Ball;
+    public GameObject BallPrefab;
 
-    private Rigidbody2D rigidbody2DComponent;
+    private Rigidbody2D Rigidbody2DComponent;
+    private SpriteRenderer SpriteComponent;
+
+    private GameObject Ball;
+
+    private bool BallLaunched;
+
+    public Vector2 GetBallInitialPosition()
+    {
+        return new Vector2(transform.position.x, transform.position.y + SpriteComponent.bounds.size.y);
+    }
 
 	// Use this for initialization
 	private void Start ()
     {
-        rigidbody2DComponent = GetComponent<Rigidbody2D>();
+        BallLaunched = false;
+
+        Rigidbody2DComponent = GetComponent<Rigidbody2D>();
+        SpriteComponent = GetComponent<SpriteRenderer>();
+
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y + SpriteComponent.bounds.size.y);
+        Ball = Instantiate(BallPrefab, newPosition, transform.rotation) as GameObject;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +41,13 @@ public class MainCharacterController : MonoBehaviour
         // Read the inputs.
         float h = CrossPlatformInputManager.GetAxis("Horizontal");
         // Move the character
-        rigidbody2DComponent.velocity = new Vector2(h * MaxVelocity, rigidbody2DComponent.velocity.y);
+        Rigidbody2DComponent.velocity = new Vector2(h * MaxVelocity, Rigidbody2DComponent.velocity.y);
+
+        if (!BallLaunched && (h != 0.0f))
+        {
+            Ball.GetComponent<MainCharacterBallController>().LaunchBall();
+
+            BallLaunched = true;
+        }
     }
 }
